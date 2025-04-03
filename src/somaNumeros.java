5d0f1c215e2c6b541bf3a4c7b2be22e2d05fd132
nicolasklange <nklange986@gmail.com>
2025-04-03 20:51:23 -0300

    first commit

---
+++ src/somaNumeros.java
@@ -0,0 +1,11 @@
+/*
+    1 - Crie um programa em que você receberá alguns números fornecidos pelo usuário,
+    enquanto ele quiser continuar oferecendo números, e, ao final, exibirá a soma dos números
+    oferecidos pelo usuário.
+*/
+
+public class somaNumeros {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
