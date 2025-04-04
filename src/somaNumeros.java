2503ff04b2ff0ac09abbb306f84fcdcb40d5df3f
nicolasklange <nklange986@gmail.com>
2025-04-03 21:05:15 -0300

    Exercicio para somar números concluido

--- src/somaNumeros.java
+++ src/somaNumeros.java
@@ -4,8 +4,38 @@
     oferecidos pelo usuário.
 */
 
+import java.util.Scanner;
+
 public class somaNumeros {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        // Variaveis
+        boolean sair = true;
+        int num = 1, soma = 0;
+
+        while (sair == true) {
+            // Entrada de dados, números para somar
+            System.out.print("Digite o " + num + "° número: ");
+            int numero = scanner.nextInt();
+
+            // Fazendo a soma dos números
+            soma += numero;
+            // Pedindo ao usuário se deseja sair
+            System.out.print("Deseja sair? (S/N): ");
+            char saida = Character.toUpperCase(scanner.next().charAt(0));
+
+            // Verificando se o usuário quer sair ou não
+            if (saida == 'S') {
+                sair = false;
+            } else {
+                sair = true;
+            }
+            
+            num++;
+
+        }
+        // Saida de dados com a soma dos números
+        System.out.println("Soma: " + soma);
+        scanner.close();
     }
 }
