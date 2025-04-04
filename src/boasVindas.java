cc2cef517e0cf16a94b1051ea3d3bd7cf5d775ed
nicolasklange <nklange986@gmail.com>
2025-04-03 21:44:52 -0300

    exercicio 5, boas vindas concluido

---
+++ src/boasVindas.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class boasVindas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variaveis
+        int homens = 0;
+        int mulheres = 0;
+
+        // Entrada de dados, quantidade de pessoas
+        System.out.print("Quantas pessoas? ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine(); 
+
+        // Laço de repetição para dar boas vindas e somar a quantidade de cumprimentos
+        for (int i = 0; i < quantidade; i++) {
+            // Entrada de dados, sexo do usuário
+            System.out.print("Informe seu sexo (f/m): ");
+            char sexo = scanner.nextLine().toLowerCase().charAt(0); 
+
+            // Nome do usuário
+            System.out.print("Informe seu nome: ");
+            String nome = scanner.nextLine();
+
+            // Verifação de sexo para fazer a soma de comprimentos para cada um
+            if (sexo == 'm') {
+                System.out.println("Bem vindo sr. " + nome);
+                homens++;
+            } else if (sexo == 'f') {
+                System.out.println("Bem vinda sra. " + nome);
+                mulheres++;
+            } else {
+                System.out.println("Sexo inválido. Saudação padrão: Bem vindo(a) " + nome);
+            }
+        }
+
+        // Saida de dados, cumprimentos de cada sexo
+        System.out.println("\nHomens cumprimentados: " + homens);
+        System.out.println("Mulheres cumprimentadas: " + mulheres);
+
+        scanner.close();
+    }
+}
