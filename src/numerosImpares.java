58d4b18fadf23846c1cbaeadf05379334d767114
nicolasklange <nklange986@gmail.com>
2025-04-03 21:10:25 -0300

    exercicio 2, números ímpares concluido

---
+++ src/numerosImpares.java
@@ -0,0 +1,23 @@
+/*
+    2 - Crie um programa que receberá um número do usuário e, em seguida, deverá imprimir
+    no console todos os números ímpares de um até esse número.
+
+ */
+
+import java.util.Scanner;
+
+public class numerosImpares {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada de dados, número para imprimir os impares até o mesmo
+        System.out.print("Informe um valor: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= numero; i+= 2) {
+            System.out.println(i + " ");
+        }
+
+        scanner.close();
+    }
+}
